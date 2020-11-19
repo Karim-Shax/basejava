01230ca249f1835ef599e2d856ac15efdd59d75b
Karim <karim.waxabai@mail.ru>
2020-11-19 21:43:57 +0600

    "change mistake"

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,30 +3,30 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = size();
 
     void clear() {
-        int size = size();
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        storage[size++] = r;
     }
 
     Resume get(String uuid) {
-        for (Resume r : storage) {
-            if (r != null && r.uuid.equals(uuid)) {
-                return r;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
-        return new Resume();
+        return null;
     }
 
     void delete(String uuid) {
         int mark = 0;
-        int size = size();
         if (size == 0 && storage[0] == null || storage[0].uuid == null) {
             return;
         }
@@ -35,10 +35,14 @@ public class ArrayStorage {
                 mark = i;
             }
         }
-        for (int i = mark; i < size; i++) {
-            int tmp = i;
-            storage[i] = storage[i + 1];
-            storage[i + 1] = storage[tmp];
+        if (storage[mark].uuid.equals(uuid)) {
+            for (int i = mark; i < size; i++) {
+                int tmp;
+                tmp = i;
+                storage[i] = storage[i + 1];
+                storage[i + 1] = storage[tmp];
+            }
+            size--;
         }
     }
 
@@ -46,22 +50,17 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] allResume = new Resume[size()];
-        int i = 0;
-        int size = size();
-        for (Resume r : storage) {
-            if (r != null && i != size) {
-                allResume[i] = r;
-                i++;
-            }
+        Resume[] allResume = new Resume[size];
+        for (int j = 0; j < size; j++) {
+            allResume[j] = storage[j];
         }
         return allResume;
     }
 
     int size() {
         int size = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {             //Проверка на null идет последовательно т.к нельзя добавлять по индексу,
+        for (Resume resume : storage) {
+            if (resume != null) {             //Проверка на null идет последовательно т.к нельзя добавлять по индексу,
                 size++;                           // и при удалений все элементы смещаются в лево,
             } else {                               // подсчет закончится при нахождений первого нулевого элемента
                 break;
