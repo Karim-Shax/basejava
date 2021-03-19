4a1b3f5bfaed4bcdde54940b2d8ca82130781d51
KARIM-Shakh <karim.waxabai@mail.ru>
2021-03-20 01:13:30 +0600

    "HW12 Stream Api tasks"

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -1,18 +1,25 @@
 package com.urise.webapp;
 
 
+import java.util.*;
+
 public class MainConcurrency {
 
     public static void main(String[] args) {
         Object lock = new Object();
         Object lock2 = new Object();
-
-        new Thread(() -> {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i < 105; i++) {
+            list.add(i);
+        }
+        System.out.println(minValue(new int[]{10, 11, 9, 5, 6, 3, 2, 7}));
+        System.out.println(oddOrEven(list));
+       /* new Thread(() -> {
             deadLock(lock, lock2);
         }).start();
         new Thread(() -> {
             deadLock(lock2, lock);
-        }).start();
+        }).start();*/
     }
 
     public static void deadLock(Object obj, Object obj2) {
@@ -28,6 +35,32 @@ public class MainConcurrency {
             synchronized (obj2) {
                 System.out.println(name + " blocked " + toStr);
             }
+        }
+    }
+
+    public static int minValue(int[] values) {
+        Optional<Integer> ar = Arrays.stream(values).boxed().filter(x -> x < 10)
+                .distinct().min(Comparator.comparingInt(x -> x));
+        return ar.get();
+    }
+
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        List<Integer> odd = new ArrayList<>();
+        List<Integer> even = new ArrayList<>();
+        int sum = integers.stream().peek(x -> {
+            if (x % 2 == 0) {
+                even.add(x);
+            } else {
+                odd.add(x);
+            }
+        }).reduce(Integer::sum).get();
+
+        if (sum % 2 == 0) {
+            System.out.println("sum is even=" + sum);
+            return odd;
+        } else {
+            System.out.println("sum is odd=" + sum);
+            return even;
         }
     }
 }
